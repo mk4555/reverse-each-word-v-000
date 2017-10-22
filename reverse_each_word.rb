4a3decc35cc2189ad7881aaf6b4bd0ad0007c8a6
Min Kim <mk4555@nyu.edu>
2017-10-22 17:11:49 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,13 +2,14 @@ def reverse_each_word(word)
   arr = word.split
   str = ""
 
-  arr.each do |a|
-    str << a.reverse + " "
-  end
+  # arr.each do |a|
+  #   str << a.reverse + " "
+  # end
+  
   modified = arr.collect do |w|
     "#{w.reverse}"
   end
-  modified.join(" ")
+  str = modified.join(" ")
   modified.chop
   # str.chop
 end
