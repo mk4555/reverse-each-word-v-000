fdacf8ca483694fb02a16d28fb6172d07bbe045f
Min Kim <mk4555@nyu.edu>
2017-10-22 17:12:25 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,10 +5,11 @@ def reverse_each_word(word)
   # arr.each do |a|
   #   str << a.reverse + " "
   # end
-
+  # str.chop
+  
   modified = arr.collect do |w|
     "#{w.reverse}"
   end
   str = modified.join(" ")
-  # str.chop
+  
 end
